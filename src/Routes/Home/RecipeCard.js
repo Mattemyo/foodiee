33ef095e1b797c15cf5fcf8d9e1791e4baaef6cf
Mattemyo <matias97marti@gmail.com>
2018-02-25 20:06:22 +0100

    fix animation

--- src/Routes/Home/RecipeCard.js
+++ src/Routes/Home/RecipeCard.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const RecipeCard = ({ recipe }) => {
-  const ingredients = recipe.ingredientLines.map(ingredient => (
-    <li key={Math.random()}>{ingredient}</li>
-  ));
+class RecipeCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayClass: 'recipe-card fade-in'
+    };
+  }
+  componentWillMount() {
+    this.setState({ displayClass: 'recipe-card' });
+  }
 
-  return (
-    <div className="recipe-card">
-      <div className="recipe-name">
-        <h3>{recipe.label}</h3>
+  render() {
+    const { recipe } = this.props;
+
+    const ingredients = recipe.ingredientLines.map(ingredient => (
+      <li key={Math.random()}>{ingredient}</li>
+    ));
+
+    return (
+      <div className={this.state.displayClass}>
+        <div className="recipe-name">
+          <h3>{recipe.label}</h3>
+        </div>
+        <div className="recipe-image">
+          <img
+            alt={recipe.label}
+            src={recipe.image}
+            style={{ borderRadius: '5px' }}
+          />
+        </div>
+        <div className="ingredients">
+          <ul>{ingredients}</ul>
+        </div>
+        <div>
+          <a target="_blank" href={recipe.url}>
+            See full recipe
+          </a>
+        </div>
       </div>
-      <div className="recipe-image">
-        <img
-          alt={recipe.label}
-          src={recipe.image}
-          style={{ borderRadius: '5px' }}
-        />
-      </div>
-      <div className="ingredients">
-        <ul>{ingredients}</ul>
-      </div>
-      <div>
-        <a target="_blank" href={recipe.url}>
-          See full recipe
-        </a>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default RecipeCard;
